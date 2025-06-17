3046c20e09e77c381aa553889064e66c4cfa28c8
DaiGuard <zenith.or.w@gmail.com>
2025-06-18 00:50:12 +0900

    マルチコントローラのシリアルNo確認処理追加

--- custack-robo-unity/Assets/Scripts/MultiPlayer/PlayerAssigner.cs
+++ custack-robo-unity/Assets/Scripts/MultiPlayer/PlayerAssigner.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -6,7 +7,19 @@ public class PlayerAssigner : MonoBehaviour
 {
     private PlayerInputManager _playerInputManager;
 
-    void Awake()
+    [System.Serializable]
+    private struct SerialPlayerPair
+    {
+        public string serialNumber;
+        public int playerId;
+    }
+
+    [SerializeField]
+    private List<SerialPlayerPair> _devicePlayerMapping;
+
+    private Dictionary<string, int> _devicePlayerDictionary = new();
+
+    void Start()
     {
         _playerInputManager = GetComponent<PlayerInputManager>();
         if (_playerInputManager == null)
@@ -15,23 +28,79 @@ public class PlayerAssigner : MonoBehaviour
             return;
         }
 
-        InputSystem.onDeviceChange += OnDeviceChange;
+        foreach (var item in _devicePlayerMapping)
+        {
+            _devicePlayerDictionary.Add(item.serialNumber, item.playerId);
+        }
 
+    }
+
+    void Update()
+    {
+        if (_playerInputManager != null)
+        {
+            if (_playerInputManager.playerCount >= _playerInputManager.maxPlayerCount
+            && _playerInputManager.maxPlayerCount > 0)
+            {
+                return;
+            }
+
+            foreach (var gamepad in Gamepad.all)
+            {
+                if (gamepad.selectButton.wasPressedThisFrame)
+                {
+                    var serial = gamepad.description.serial;
+                    Debug.Log($"{serial} pressed!!");
+                }
+            }    
+        }
     }
 
     public void OnPlayerJoined(PlayerInput playerInput)
     {
-        Debug.Log($"{playerInput.devices} joined!");
+        var device = playerInput.devices.Count > 0 ? playerInput.devices[0] : null;
+        if (device == null)
+        {
+            Debug.LogError("not found device");
+            return;
+        }
+
+        if (device is Gamepad)
+        {
+            var serial = device.description.serial;
+            if (_devicePlayerDictionary.ContainsKey(serial))
+            {
+                var playerId = _devicePlayerDictionary[serial];
+                Debug.Log($"{serial}: {playerId} add!!");
+
+                return;
+            }
+        }
+
+        Destroy(playerInput.gameObject);
+
+        return;
     }
 
     public void OnPlayerLeft(PlayerInput playerInput)
     {
-        Debug.Log($"{playerInput.devices} left!");
-    }
+        var device = playerInput.devices.Count > 0 ? playerInput.devices[0] : null;
+        if (device == null)
+        {
+            Debug.LogError("not found device");
+            return;
+        }
 
-    public void OnDeviceChange(InputDevice device, InputDeviceChange change)
-    {
-        
-        Debug.Log($"{device.description.serial} change!!");
+        if (device is Gamepad)
+        {
+            var serial = device.description.serial;
+            if (_devicePlayerDictionary.ContainsKey(serial))
+            {
+                var playerId = _devicePlayerDictionary[serial];
+                Debug.Log($"{serial}: {playerId} add!!");
+
+                return;
+            }
+        }
     }
 }

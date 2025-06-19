181916bcbf414940ed208ce975adf0e0b0833a1d
DaiGuard <zenith.or.w@gmail.com>
2025-06-20 08:49:42 +0900

    武器調整中

--- custack-robo-unity/Assets/Scripts/MultiPlayer/PlayerMove.cs
+++ custack-robo-unity/Assets/Scripts/MultiPlayer/PlayerMove.cs
@@ -4,32 +4,63 @@ using UnityEngine.InputSystem;
 [RequireComponent(typeof(PlayerInput))]
 public class PlayerMove : MonoBehaviour
 {
-    private PlayerInput playerInput;
+    [SerializeField]
+    private GameObject _attackEffectPrefab;
+    
+    private PlayerInput _playerInput;
+    private Vector2 _moveInput;
 
     void Awake()
     {
-        playerInput = GetComponent<PlayerInput>();
+        _playerInput = GetComponent<PlayerInput>();
     }
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        if (playerInput != null)
+        if (_playerInput != null)
         {
-            print($"player start: {playerInput.user.id}");
+            print($"player start: {_playerInput.user.id}");
         }
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (playerInput == null)
+        if (_playerInput == null)
         {
             return;
         }
 
-        Vector2 moveInput = playerInput.actions["Move"].ReadValue<Vector2>();
-        Vector3 movement = new Vector3(moveInput.x, moveInput.y, 0f);
+        Vector3 movement = new Vector3(_moveInput.x, _moveInput.y, 0f);
         transform.Translate(movement * 2.0f * Time.deltaTime);
-    }        
+    }
+
+    public void MoveEvent(InputAction.CallbackContext context)
+    {
+        if (context.performed)
+        {
+            _moveInput = context.ReadValue<Vector2>();
+        }
+        else if (context.canceled)
+        {
+            _moveInput = Vector2.zero;
+        }
+    }
+
+    public void AttackEvent(InputAction.CallbackContext context)
+    {
+        if (context.started)
+        {
+
+        }
+        else if (context.performed)
+        {
+
+        }
+        else if (context.canceled)
+        {
+
+        }
+    }
 }

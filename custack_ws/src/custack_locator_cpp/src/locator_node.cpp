e6c3f14120255ece82c41cf4987eab81474445c0
DaiGuard <zenith.or.w@gmail.com>
2026-05-05 12:11:46 +0900

    ros_tcp_endpointと通信するため/robot_posesのQoSを変更 best_effort -> reliable レイテンシを後で確認する

--- custack_ws/src/custack_locator_cpp/src/locator_node.cpp
+++ custack_ws/src/custack_locator_cpp/src/locator_node.cpp
@@ -128,8 +128,10 @@ LocatorNode::LocatorNode() : rclcpp::Node("locator_node") {
     image_pub_ = this->create_publisher<sensor_msgs::msg::Image>(camera_topic, image_qos);
 
     auto robot_pose_qos = rclcpp::QoS(3)
-        .best_effort()
+        // .best_effort()
+        .reliable()
         .durability_volatile();
+        
     robot_pose_pub_ = this->create_publisher<custack_msgs::msg::RobotPoseArray>(
         robot_pose_topic, robot_pose_qos);
 
@@ -491,10 +493,10 @@ void LocatorNode::processCallback() {
     }
     robot_pose_pub_->publish(robot_poses);
 
-    cv::Mat preview_frame;
-    cv::resize(processed_frame_, preview_frame, cv::Size(), 0.5, 0.5, cv::INTER_AREA);
-    cv::imshow("Processed Frame", preview_frame);
-    cv::waitKey(1);
+    //cv::Mat preview_frame;
+    //cv::resize(processed_frame_, preview_frame, cv::Size(), 0.5, 0.5, cv::INTER_AREA);
+    //cv::imshow("Processed Frame", preview_frame);
+    //cv::waitKey(1);
 }
 
 void LocatorNode::infoCallback() {
